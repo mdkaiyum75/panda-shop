b475971b9ab57af9cd7eb7f6e4d8279b5ac33867
MD KAIYUM HOSSAIN <mdkaiyumhossain75@gmail.com>
2023-04-15 00:30:03 +0600

    NEW CHANGES

---
+++ script.js
@@ -0,0 +1,11 @@
+const close = document.getElementById("close");
+
+close.addEventListener("click",() => window.close())
+
+let number = 2;
+
+
+for( let i = 0; i< number;i++){
+    window.open("https://www.youtube.com", "", "width=1000,height=1100");
+
+}
